a8c9fad3ae7b71496993f1ec9f99f8eea1d7d2c3
Jude Daniel Chigozie <79247854+dajuctech@users.noreply.github.com>
2026-01-15 21:28:52 +0000

    Complete Docker and PostgreSQL data pipeline workshop

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # data-engineering-zoomcamp
 data engineering zoomcamp codespaces
+
+## Docker and PostgreSQL Workshop
+
+Learned how to:
+- Set up a data ingestion pipeline using Python and Docker
+- Use Docker Compose to orchestrate PostgreSQL and pgAdmin containers
+- Ingest CSV data into PostgreSQL in chunks using pandas and SQLAlchemy
+- Manage Python dependencies with UV in a containerized environment
+- Create reproducible data pipelines with multi-stage Docker builds
--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,27 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
+import click
 import pandas as pd
-
-# Read a sample of the data
-prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-df = pd.read_csv(prefix + '/yellow_tripdata_2021-01.csv.gz', nrows=100)
-
-# Display first rows
-df.head()
-
-# Check data types
-df.dtypes
-
-# Check data shape
-df.shape
-
-
-# In[2]:
-
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
 
 dtype = {
     "VendorID": "Int64",
@@ -42,175 +25,63 @@ dtype = {
     "congestion_surcharge": "float64"
 }
 
-parse_dates = [
-    "tpep_pickup_datetime",
-    "tpep_dropoff_datetime"
-]
-
-df = pd.read_csv(
-    prefix + '/yellow_tripdata_2021-01.csv.gz',
-    ##nrows=100
-    dtype=dtype,
-    parse_dates=parse_dates
-)
-
-
-# In[3]:
-
-
-len(df)
-
-
-# In[4]:
-
-
-df.head()
-
-
-# In[5]:
-
-
-
-
-# In[6]:
-
-
-# how to connect to the database
-from sqlalchemy import create_engine
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
-
-
-# In[7]:
-
-
-
-
-# In[ ]:
-
-
-#df.to_sql(name='yellow_taxi_data', con=engine)
-df.to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
-
-
-# In[ ]:
-
-
-# schema that will be created inside the database
-print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
-
-
-# In[ ]:
-
-
-# Create the table:
-df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
-
-
-# In[ ]:
-
-
-# Define variables
-df_iter = pd.read_csv(
-    url,
-    dtype=dtype,
-    parse_dates=parse_dates,
-    iterator=True,
-    chunksize=100000
-)
-
-
-# In[ ]:
-
-
-df_iter
-
-
-# In[ ]:
-
-
-for df_chunk in df_iter:
-    print(len(df_chunk))
-
-
-# In[ ]:
-
-
-# Define variables
-import pandas as pd
-from sqlalchemy import create_engine
-
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
-
-url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz'
-
-dtype = {
-    'VendorID': 'Int64',
-    'passenger_count': 'Int64',
-    'trip_distance': 'float64',
-    'RatecodeID': 'Int64',
-    'store_and_fwd_flag': 'object',
-    'PULocationID': 'Int64',
-    'DOLocationID': 'Int64',
-    'payment_type': 'Int64',
-    'fare_amount': 'float64',
-    'extra': 'float64',
-    'mta_tax': 'float64',
-    'tip_amount': 'float64',
-    'tolls_amount': 'float64',
-    'improvement_surcharge': 'float64',
-    'total_amount': 'float64',
-    'congestion_surcharge': 'float64'
-}
-
-parse_dates = ['tpep_pickup_datetime', 'tpep_dropoff_datetime']
-
-
-# In[ ]:
-
-
-# Create iterator
-df_iter = pd.read_csv(
-    url,
-    dtype=dtype,
-    parse_dates=parse_dates,
-    iterator=True,
-    chunksize=100000
-)
-
-
-# In[ ]:
-
-
-# Process chunks
-for df_chunk in df_iter:
-    print(f"Processing chunk with {len(df_chunk)} rows")
-    df_chunk.to_sql(name='yellow_taxi_data', con=engine, if_exists='append', index=False)
-    print("Chunk inserted")
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-# insert into the database
-from tqdm.auto import tqdm
-
-
-# In[ ]:
-
-
-from tqdm.auto import tqdm
-
-for df_chunk in tqdm(df_iter):
-    df_chunk.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
-
-
-# In[ ]:
-
-
-
-
+parse_dates = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
+
+
+def ingest_data(url, engine, target_table, chunksize=100000):
+    """Ingest data from CSV to PostgreSQL in chunks"""
+    df_iter = pd.read_csv(
+        url, dtype=dtype, parse_dates=parse_dates,
+        iterator=True, chunksize=chunksize
+    )
+
+    first_chunk = next(df_iter)
+    
+    # Create table
+    first_chunk.head(0).to_sql(
+        name=target_table, con=engine, if_exists="replace"
+    )
+    print(f"Table {target_table} created")
+
+    # Insert first chunk
+    first_chunk.to_sql(
+        name=target_table, con=engine, if_exists="append"
+    )
+    print(f"Inserted first chunk: {len(first_chunk)}")
+
+    # Insert remaining chunks
+    for df_chunk in tqdm(df_iter):
+        df_chunk.to_sql(
+            name=target_table, con=engine, if_exists="append"
+        )
+
+    print(f'Data ingestion to {target_table} completed!')
+
+
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table')
+def main(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
+    """Main ingestion function"""
+    # Create engine
+    engine = create_engine(
+        f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}'
+    )
+    
+    # Build URL
+    url_prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
+    url = f'{url_prefix}/yellow_tripdata_{year:04d}-{month:02d}.csv.gz'
+    
+    # Run ingestion
+    ingest_data(url=url, engine=engine, target_table=target_table, chunksize=chunksize)
+
+
+if __name__ == '__main__':
+    main()
